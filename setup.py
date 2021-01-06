e54a6298704d4217bd5125b058cfc26edb4b82a4
Stephen L <lrq3000@gmail.com>
2021-01-06 02:54:07 +0100

    bump version 1.0.4
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ except ImportError:
 
 setup(
     name = "unireedsolomon",
-    version = "1.0.3",
+    version = "1.0.4",
     description = "Universal errors-and-erasures Reed Solomon codec (error correcting code) in pure Python with extensive documentation",
     author = "Andrew Brown, Stephen Larroque",
     author_email = "lrq3000@gmail.com",
